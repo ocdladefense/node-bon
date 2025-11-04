40e661073450dea56efb1b729de57b5679352835
José Bernal <jbernal.web.dev@gmail.com>
2025-11-04 15:05:32 -0800

    Format book covers for mobile.

--- src/components/BookCovers.jsx
+++ src/components/BookCovers.jsx
@@ -24,9 +24,9 @@ export default function BookCovers() {
 
         {books.map(book => {
             return (
-                <div className="tablet:col-span-4 col-span-6">
+                <div style={{ height: "30%" }} className="tablet:col-span-4 col-span-6">
                     <a className="cursor-pointer" onClick={() => navigate(`/book/${book.shortName}/1`)}>
-                        <div style={{ color: '#fff', width: '400px', height: '480px', textAlign: 'center', marginBottom: '4px', backgroundImage: `url(/images/covers/${book.shortName}.png)`, backgroundSize: 'cover' }} className="bg-ocdla-dark-blue px-4 rounded-lg shadow-lg hover:shadow-2xl transition-shadow duration-300">&nbsp;</div>
+                        <div style={{ color: '#fff', textAlign: 'center', marginBottom: '4px', backgroundImage: `url(/images/covers/${book.shortName}.png)`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }} className="min-h-[200px] tablet:min-h-[480px] bg-ocdla-dark-blue px-4 rounded-lg shadow-lg hover:shadow-2xl transition-shadow duration-300">&nbsp;</div>
                     </a>
                     <strong>{book.name}</strong>
                 </div>
