ddf72fd9411829c41352815cac92f40c9c84882f
José Bernal <jbernal.web.dev@gmail.com>
2025-11-03 21:27:42 -0800

    Render authors as part of ChapterContents component.

--- src/components/ChapterContents.jsx
+++ src/components/ChapterContents.jsx
@@ -30,10 +30,10 @@ export default function ChapterContents({ label, name, authors }) {
 
             {content ?
                 <div className="video-content relative w-full">
-                    <h2 className="text-3xl font-bold my-0">{label} - {name}</h2>
-                    <h3 className="my-0">{authors}</h3>
-                    <h2 style={{ borderRadius: "0px 0px 8px 8px", zIndex: "100" }} className="my-0 sticky top-0 p-4 bg-ocdla-dark-blue text-white">{bookId.toUpperCase()} | {label} - {name}</h2>
-                    <div dangerouslySetInnerHTML={{ __html: content }} />
+                    <h2 className="text-3xl font-bold my-2">{label} - {name}</h2>
+                    <h3 className="text-xl font-italic my-1">{authors}</h3>
+                    <h2 style={{ borderRadius: "0px 0px 8px 8px", zIndex: "100" }} className="my-0 mt-8 sticky top-0 p-4 bg-ocdla-dark-blue text-white">{bookId.toUpperCase()} | {label} - {name}</h2>
+                    <div className="mt-8" dangerouslySetInnerHTML={{ __html: content }} />
                 </div>
                 : ""}
 
--- src/js/utils/book.js
+++ src/js/utils/book.js
@@ -58,8 +58,8 @@ export async function getChapterMetadata(bookId, chapterId) {
     let elem = index.getElementById(`${bookId}-${chapterId}`);
     const label = elem.getAttribute("label");
     const name = elem.getAttribute("name");
-    const authors = elem.getAttribute("authors");
-
+    const authors = elem.querySelector("meta[name='authors']").getAttribute("content");
+    console.log(elem.children);
     console.log(label, name, authors);
 
     return {
