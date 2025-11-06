73248689f30d856c9149b4403dc41857607f6a86
José Bernal <jbernal.web.dev@gmail.com>
2025-11-06 10:37:32 -0800

    Format correct URLs for appendices.

--- src/components/ui/Appendices.jsx
+++ src/components/ui/Appendices.jsx
@@ -7,13 +7,23 @@
 export default function Appendices({ files }) {
     return (
         <div className="space-y-4">
-            {files.map(file => (
-                <div key={file.name} className="p-4 border border-gray-300 rounded-lg hover:shadow-lg transition-shadow duration-300">
-                    <a href={file.url} className="text-blue-600 hover:underline" target="_blank" rel="noopener noreferrer">
+            {files.map(file => {
+
+                let parts = file.id.split('-');
+                let book = parts.shift();
+                let chapter = parts.shift();
+
+                let path = "/data/" + book + "/appendices/" + [book, chapter].join('-') + "/";
+                let url = path + file.url;
+                console.log(parts, book, chapter);
+
+
+                return (<div key={file.name} className="p-4 border border-gray-300 rounded-lg hover:shadow-lg transition-shadow duration-300">
+                    <a href={url} className="text-blue-600 hover:underline" target="_blank" rel="noopener noreferrer">
                         Appendix {file.id.split('-')[2].toUpperCase()}: {file.name}
                     </a>
-                </div>
-            ))}
+                </div>)
+            })}
         </div>
     );
 }
